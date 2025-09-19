50d1ceb51e4dbda9d803c7a9e28903f33f637bc4
Alex McCullough <alex.mccullough@kainos.com>
2025-09-19 15:44:42 +0100

    feat: more robust db seeding

--- src/main/java/com/kainos/Application.java
+++ src/main/java/com/kainos/Application.java
@@ -1,9 +1,7 @@
 package com.kainos;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,13 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner seedDatabase(EmployeeRepository repository) {
-        return args -> {
-            repository.save(new Employee("Alice", "Smith", Band.ASSOCIATE, "Developer", 50000));
-            repository.save(new Employee("Bob", "Johnson", Band.MANAGER, "Designer", 45000));
-        };
     }
 }
---
+++ src/main/java/com/kainos/DatabaseSeeder.java
@@ -0,0 +1,40 @@
+package com.kainos;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
+
+@Configuration
+public class DatabaseSeeder {
+
+    @Bean
+    public CommandLineRunner seedDatabase(EmployeeRepository repository) {
+        return args -> {
+            Random rand = new Random();
+            String[] firstNames = {"Alex", "Stephen", "Jacob", "Mary", "Bob", "Alice", "Karen", "Samuel", "Lee", "Dylan"};
+            String[] lastNames = {"Smith", "Johnson", "Williams", "Brown", "Jones", "Miller", "Davis", "Garcia", "Rodriguez", "Wilson"};
+            String[] jobRoles = {"Software Engineer", "Project Manager", "Data Analyst", "Product Owner", "UX Designer", "QA Tester", "DevOps Engineer", "Business Analyst"};
+            Band[] enumValues = Band.values();
+
+            final int NUM_TO_CREATE = 25;
+            final int MIN_SALARY_FACTOR = 19;
+            final int MAX_SALARY_FACTOR = 450;
+
+            for (var employee : repository.findAll()) {
+                repository.delete(employee);
+            }
+
+            for (var i = 0; i < NUM_TO_CREATE; i++) {
+                String firstName = firstNames[rand.nextInt(0, firstNames.length - 1)];
+                String lastName = lastNames[rand.nextInt(0, lastNames.length - 1)];
+                Band band = enumValues[rand.nextInt(0, enumValues.length - 1)];
+                String jobRole = jobRoles[rand.nextInt(0, jobRoles.length - 1)];
+                int salary = rand.nextInt(MIN_SALARY_FACTOR, MAX_SALARY_FACTOR) * 1000;
+
+                repository.save(new Employee(firstName, lastName, band, jobRole, salary));
+            }
+        };
+    }
+}
