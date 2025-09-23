b15dd1b9026a997321c1f16128cf56ede2189dc5
Alex McCullough <alex.mccullough@kainos.com>
2025-09-23 09:02:51 +0100

    refactor: no longer using deprecrated signWith method in JwtUtil

--- src/main/java/com/kainos/JwtUtil.java
+++ src/main/java/com/kainos/JwtUtil.java
@@ -1,21 +1,22 @@
 package com.kainos;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private String secret = "myverysecretkey";
+    private final Key key = Keys.hmacShaKeyFor("my-very-secret-256-bit-secret-key".getBytes());
 
     public String generateJwtToken(String username) {
         return Jwts.builder()
                 .subject(username)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
-                .signWith(SignatureAlgorithm.HS256, secret)
+                .signWith(key)
                 .compact();
     }
 }
